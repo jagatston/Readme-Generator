760f0f2cd7760b3dce76bd43dad9a21861d09b88
Jonathan Agatston <jagatston123@gmail.com>
2021-04-09 20:26:16 -0400

    pulled code from mini project to compare to hw

---
+++ Develop/ExampleREADME.md
@@ -0,0 +1,50 @@
+# jajaja
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/jajaja/jajaja?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/jajaja/jajaja?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  jajaja
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  jajaj
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  jaja
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  jajaj
+  
+  ## License
+  
+  undefined
+  
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](https://avatars.githubusercontent.com/u/803891?v=4) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@jajaja](https://api.github.com/users/jajaja)
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,92 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Inquirer prompts for userResponses
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
 
-// Function call to initialize app
+const writeFileAsync = util.promisify(writeToFile);
+
+
+// Main function
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 init();
